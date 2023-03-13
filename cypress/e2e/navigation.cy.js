06417f21bca377c91a5e0115e981743df42d47e0
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2023-03-13 01:34:33 +0100

    Test Page Navigation

---
+++ cypress/e2e/navigation.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe("Page Navigation", () => {
+  it("should navigate between pages", () => {
+    cy.visit('http://127.0.0.1:5173/');
+    cy.get('[data-cy="header-about-link"]').click();
+    cy.location("pathname").should("eq", "/about");   // /about => About page
+
+    cy.go("back");
+    cy.location("pathname").should("eq", "/");    // / => Home page
+
+    cy.get('[data-cy="header-about-link"]').click();
+    cy.get('[data-cy="header-home-link"]').click();
+    cy.location("pathname").should("eq", "/");    // / => Home page
+  });
+});

19f6281ddfc208e1f827245bee11c1116c092a87
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2023-03-13 01:57:47 +0100

    Work with Values and Aliases

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -7,10 +7,13 @@ describe("Contact Form", () => {
     cy.get('[data-cy="contact-input-name"]').type("John Doe");
     cy.get('[data-cy="contact-input-email"]').type("test@example.com");
 
-    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
-    cy.get('[data-cy="contact-btn-submit"]').should("not.have.attr", "disabled");
-    cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
-    cy.get('[data-cy="contact-btn-submit"]').should("have.attr", "disabled");
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains("Send Message")
+      .should("not.have.attr", "disabled");
+
+    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+    cy.get("@submitBtn").click()
+      .contains("Sending...")
+      .should("have.attr", "disabled");
   });
 });

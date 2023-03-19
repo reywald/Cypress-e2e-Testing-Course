27ae6db3ed4fafdf1f4aea98c70e7fa51d31ec59
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2023-03-19 17:27:18 +0100

    Create a Custom Command

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -13,7 +13,9 @@ describe("Contact Form", () => {
       expect(el.attr("disabled")).to.be.undefined;
       expect(el.text()).to.eq("Send Message");
     });
-    cy.get('[data-cy="contact-input-email"]').type("test@example.com{enter}");
+    cy.get('[data-cy="contact-input-email"]').type("test@example.com");
+    cy.submitForm();
+
     cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
     cy.get("@submitBtn").click()
       .contains("Sending...")
@@ -21,7 +23,7 @@ describe("Contact Form", () => {
   });
 
   it("should validate the form input", () => {
-    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.submitForm();
     cy.get('[data-cy="contact-btn-submit"]').then(el => {
       expect(el).to.not.have.attr("disabled");
       expect(el.text()).to.not.equal("Sending...");
